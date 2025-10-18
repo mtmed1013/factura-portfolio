f31d7f7d331fd17f063e6381e5a85f9d926cca66
mtmed1013 <mateo1013@gmail.com>
2025-10-17 20:01:33 -0500

    Se realiza avance en facturas

--- Front/fact-front/src/app/features/facts/page/facts-details-page/facts-details-page.ts
+++ Front/fact-front/src/app/features/facts/page/facts-details-page/facts-details-page.ts
@@ -1,4 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { SelectModule } from 'primeng/select';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { CardNewComponent } from '../../../../shared/components/card-new/card-new.component';
+import { DialogDeleteConfirm } from '../../../../shared/components/dialog-delete-confirm/dialog-delete-confirm';
+import { FormClients } from '../../../administration/clients/components/form-clients/form-clients';
 
 @Component({
   selector: 'app-facts-details-page',
@@ -6,6 +18,6 @@ import { Component } from '@angular/core';
   templateUrl: './facts-details-page.html',
   styleUrl: './facts-details-page.scss'
 })
-export class FactsDetailsPage {
+export class FactsDetailsPage implements OnInit  {
 
 }
--- Front/fact-front/src/app/features/facts/page/facts-page/facts-page.ts
+++ Front/fact-front/src/app/features/facts/page/facts-page/facts-page.ts
@@ -1,8 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { SelectModule } from 'primeng/select';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { CardNewComponent } from '../../../../shared/components/card-new/card-new.component';
+import { DialogDeleteConfirm } from '../../../../shared/components/dialog-delete-confirm/dialog-delete-confirm';
+import { FormClients } from '../../../administration/clients/components/form-clients/form-clients';
 
 @Component({
   selector: 'app-facts-page',
-  imports: [],
+  imports: [CommonModule, TableModule,
+    TagModule, IconFieldModule, InputIconModule,
+    InputTextModule,
+    MultiSelectModule, SelectModule, ButtonModule,
+    FormClients, CardNewComponent,
+    DialogDeleteConfirm],
   templateUrl: './facts-page.html',
   styleUrl: './facts-page.scss'
 })
